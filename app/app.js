7e05a931394210935d588bf5805a573a6c2697f1
Artsiom Shushkevich <artemshushkevich@live.com>
2018-01-30 16:59:05 +0300

    Add welcome default page

--- app/app.js
+++ app/app.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import blogsRouter from './components/blogs/blogs.router';
+import path from 'path';
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.use('/blogs', blogsRouter);
+
+app.use((req, res) => {
+    res.render('welcome');
+});
 
 export default app;
